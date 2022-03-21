ab44637a8a3018fd777f587d2bcf056b0c621ba5
Yash <yashrajmehta3027@gmail.com>
2022-03-22 00:10:24 +0530

    Home Base

--- app/src/main/java/com/gail/attendancesystem/Base.java
+++ app/src/main/java/com/gail/attendancesystem/Base.java
@@ -1,19 +1,49 @@
 package com.gail.attendancesystem;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.location.Location;
+import android.location.LocationListener;
+import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.core.content.FileProvider;
 import androidx.fragment.app.Fragment;
 
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebView;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Objects;
 
 /**
  * A simple {@link Fragment} subclass.
  * Use the {@link Base#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class Base extends Fragment {
+public class Base extends Fragment implements LocationListener, OnMapReadyCallback {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -23,6 +53,16 @@ public class Base extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+    MapView mapView;
+    ImageView image;
+    Button cap;
+    String Location;
+    Uri imageUri;
+    Bitmap bitmap;
+    String current;
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+    WebView webView;
+    String la="22.6072446",lo="72.8086444";
 
     public Base() {
         // Required empty public constructor
@@ -55,10 +95,82 @@ public class Base extends Fragment {
         }
     }
 
+    @SuppressLint("SetJavaScriptEnabled")
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_base, container, false);
+        View v=inflater.inflate(R.layout.fragment_base, container, false);
+        mapView=v.findViewById(R.id.map);
+        webView=v.findViewById(R.id.web);
+        mapView.getMapAsync(Base.this);
+        mapView.onCreate(savedInstanceState);
+        webView.loadUrl("https://www.google.com/maps/search/?api=1&query="+la+","+lo);
+        webView.getSettings().setJavaScriptEnabled(true);
+        cap=v.findViewById(R.id.cap_button);
+        image=v.findViewById(R.id.image);
+        cap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.CAMERA)!= PackageManager.PERMISSION_GRANTED){
+                    ActivityCompat.requestPermissions(getActivity(),new String[]{
+                            Manifest.permission.CAMERA
+                    },100);
+                }
+                dispatchTakePictureIntent();
+            }
+        });
+        return v;
+    }
+
+    private void dispatchTakePictureIntent() {
+        String name="photo";
+        File storage=getContext().getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        try {
+            File imageFile=File.createTempFile(name,".jpg",storage);
+            current=imageFile.getAbsolutePath();
+            imageUri= FileProvider.getUriForFile(getContext(),"com.rajaryan.analytica.fileprovider",imageFile);
+            Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT,imageUri);
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == -1) {
+            // on below line we are getting
+            // data from our bundles. .
+
+
+            bitmap= BitmapFactory.decodeFile(current);
+
+            // below line is to set the
+            // image bitmap to our image.
+            image.setImageBitmap(bitmap);
+
+
+        }
+    }
+
+    @Override
+    public void onLocationChanged(@NonNull Location location) {
+        Location="Latitude:" + location.getLatitude() + ", Longitude:" + location.getLongitude();
+        la=String.valueOf(location.getLatitude());
+        lo=String.valueOf(location.getLongitude());
+    }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+        GoogleMap mMap = googleMap;
+        // Add a marker in Sydney and move the camera
+        LatLng sydney = new LatLng(Double.parseDouble(la), Double.parseDouble(lo));
+        mMap.addMarker(new MarkerOptions()
+                .position(sydney)
+                .title("Your Location"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
 }
--- app/src/main/java/com/gail/attendancesystem/Home.java
+++ app/src/main/java/com/gail/attendancesystem/Home.java
@@ -1,8 +1,10 @@
 package com.gail.attendancesystem;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
+import android.view.View;
 
 import com.etebarian.meowbottomnavigation.MeowBottomNavigation;
 
@@ -15,6 +17,43 @@ public class Home extends AppCompatActivity {
         bottomNavigation=findViewById(R.id.bottom);
         bottomNavigation.add(new MeowBottomNavigation.Model(1, R.drawable.ic_outline_dashboard_24));
         bottomNavigation.add(new MeowBottomNavigation.Model(2, R.drawable.ic_baseline_perm_identity_24));
+        FragmentManager fragmentManager = getSupportFragmentManager();
 
+        bottomNavigation.setOnShowListener(new MeowBottomNavigation.ShowListener() {
+            @Override
+            public void onShowItem(MeowBottomNavigation.Model item) {
+
+                switch (item.getId()){
+                    case 1:
+                        fragmentManager.beginTransaction()
+                                .replace(R.id.container, Base.class, null)
+                                .setReorderingAllowed(true)
+                                .addToBackStack("name") // name can be null
+                                .commit();
+                        break;
+                    case 2:
+                        fragmentManager.beginTransaction()
+                                .replace(R.id.container, Data.class, null)
+                                .setReorderingAllowed(true)
+                                .addToBackStack("name") // name can be null
+                                .commit();
+                        break;
+                }
+
+            }
+        });
+        bottomNavigation.show(1,true);
+        bottomNavigation.setOnClickMenuListener(new MeowBottomNavigation.ClickListener() {
+            @Override
+            public void onClickItem(MeowBottomNavigation.Model item) {
+
+            }
+        });
+        bottomNavigation.setOnReselectListener(new MeowBottomNavigation.ReselectListener() {
+            @Override
+            public void onReselectItem(MeowBottomNavigation.Model item) {
+
+            }
+        });
     }
 }
